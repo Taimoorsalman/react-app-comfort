196fac0907e49617a113253c4e3b01b02b8b8e32
Taimoor <mtaimoorsalman@gmail.com>
2023-12-29 01:57:41 +0500

    commit2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Homepage from "./pages/homepage";
-
+import Layout from "./global/Layout";
 
 function App() {
   return (
     <div className="App">
+      <Layout />
       <Homepage />
+      
       
     </div>
   );
